0632ddf99e18c82b5e3a1e7420290883f5ae7e1b
Michael Wiesinger <michw2014@gmail.com>
2022-01-26 11:31:57 +0100

    update

---
+++ Chatter/Chatter/ChatWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ClockObserver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chatter
+{
+    /// <summary>
+    /// Interaktionslogik für ChatWindow.xaml
+    /// </summary>
+    public partial class ChatWindow : Window
+    {
+
+        public string Title { get; private set; }
+        private MessageSubject subject;
+       
+
+        public ChatWindow(MessageSubject subject, string title)
+        {
+            DataContext = this;
+            InitializeComponent();
+            Title = title;
+            this.subject = subject;
+        }
+    }
+}
---
+++ Chatter/Chatter/Subject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockObserver
+{
+    public class Subject
+    {
+        private List<IObserver> observers = new List<IObserver>();
+
+
+        public void Attach(IObserver observer)
+        {
+            lock (observers)
+            {
+                observers.Add(observer);
+            }
+        }
+
+        public void Detach(IObserver observer)
+        {
+            lock (observers)
+            {
+                observers.Remove(observer);
+            }
+        }
+
+        public void Notify()
+        {
+            lock (observers)
+            {
+                foreach (IObserver observer in observers)
+                {
+                    observer.Update();
+                }
+            }
+        }
+
+    }
+}
